807b5f2a0c4081956eb6107793a863e1c7b6e6e9
Leonard Frankel <leonard@leonardfrankel.com>
2016-01-30 11:28:58 +1100

    collision based clicking and pipelaying is working

---
+++ ChemPlant/Assets/Chemical.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Chemical : System.IEquatable<Chemical>
+{
+	public string name;
+	public Color colour;
+
+	public bool Equals(Chemical other) {return this.name.Equals(other.name);}
+
+	private Chemical(string name, float r, float g, float b)
+	{
+		this.name = name; 
+		colour = new Color(r, g, b);
+	}
+
+
+	public static Chemical HYDROGEN = new Chemical("Hydrogen", 255, 0, 0);
+	public static Chemical OXYGEN = new Chemical("Hydrogen", 0, 0, 255);
+	public static Chemical WATER = new Chemical("Water", 0, 100, 255);
+}
---
+++ ChemPlant/Assets/Vessel.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+/*a vessel is any thing that stores reactant. 
+
+ map of reactant to quantity.
+
+ one output, multiple inputs.
+
+ output, when active, sends all of the contents to the input of the next reactor.
+
+ what about more than one output, for splitting?.... probably not going to have time for that to matter.
+
+ ok. so, for the time being, a reactor does something to its reactants under certain conditions:
+
+*automatically
+*when the player presses the button on the reactor
+*when the player turns the valve
+
+
+
+
+ store references to the vessels you're linking TO. that is, the vessel your output will go to. create those links
+ when connecting pipe, break those links when disconnecting pipe.
+
+ later on, being fancy, could store those pipes as actual game entities rather than just cosmetics, but for now just reference what
+ you're connecting to.
+
+ Actually, they do have to be entities because they have to be "on" or not, for whether to send the chemicals through or not.  */
+
+public class Vessel : BaseClickable
+{
+	public float capacity;
+
+	/*maps chemical to quantity of that chemical*/
+	private IDictionary<Chemical, float> _contents = new Dictionary<Chemical, float>();
+
+	private ICollection<Pipe> _outputs = new LinkedList<Pipe>();
+
+	// Use this for initialization
+	void Start()
+	{
+		ClickManager.getInstance().registerClickable(this);
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		foreach(Reaction reaction in Reaction.getReactions())
+		{
+			reaction.run(_contents, Time.deltaTime);
+		}
+	}
+
+	/*for rendering pipes.
+
+	 ideally this would offset each output pipe, but for now just the one please*/
+	public Vector3 getPipeOutPoint()
+	{
+		return transform.position;
+	}
+
+	public Vector3 getPipeInPoint()
+	{
+		return transform.position;
+	}
+
+	public override void onClickedOn()
+	{
+		PipeManager.getInstance().startPipe(this);
+	}
+
+	public override void onClickRelease()
+	{
+		PipeManager.getInstance().endPipe(this);
+	}
+
+	public void onPipeConnected(Pipe pipe)
+	{
+		_outputs.Add(pipe);
+	}
+
+	//adds chemical to the contents, as much as there's room, and returns the excess.
+	public float addChemical(Chemical chem, float inputQuantity)
+	{
+		float excess = 0;
+
+		float totalQuantity = 0;
+		foreach(float quantity in _contents.Values)
+		{
+			totalQuantity += quantity;
+		}
+
+		float capacityRemaining = capacity - totalQuantity;
+
+		if(capacityRemaining < inputQuantity)
+		{
+			excess = inputQuantity - capacityRemaining;
+			inputQuantity -= capacityRemaining;
+		}
+
+		if(_contents.ContainsKey(chem))
+		{
+			_contents[chem] += inputQuantity;
+		}
+		else
+		{
+			_contents.Add(chem, inputQuantity);
+		}
+
+		return excess;
+
+	}
+	
+}
